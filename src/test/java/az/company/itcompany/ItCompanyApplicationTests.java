7dbb9fa02f2b8523a7c3edbd599ea4261fd4dab9
khayalfarzi <xeyalferziyev9@gmail.com>
2020-02-12 10:25:05 +0300

    creating initial project close #1

---
+++ src/test/java/az/company/itcompany/ItCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.itcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
